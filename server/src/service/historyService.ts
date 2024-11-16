af57ecadfbce41742b7f77a15589fbd97af6b80f
Jason Cable <cablej02@gmail.com>
2024-11-16 16:35:55 -0600

    finished HistoryService class

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,3 +1,7 @@
+//import { error } from 'node:console';
+import fs from 'node:fs/promises'
+import { v4 as uuidv4 } from 'uuid';
+
 class City{
     name: string;
     id: string;
@@ -7,18 +11,56 @@ class City{
     }
 }
 
-// TODO: Complete the HistoryService class
 class HistoryService {
-  // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
-  // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  // private async write(cities: City[]) {}
-  // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  // async getCities() {}
-  // TODO Define an addCity method that adds a city to the searchHistory.json file
-  // async addCity(city: string) {}
-  // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
-  // async removeCity(id: string) {}
+    private async read() {
+        return await fs.readFile('db/searchHistory.json', {
+            flag: 'a+', //TODO: Not sure what this is doing. look into it
+            encoding: 'utf-8'
+        })
+    }
+
+    private async write(cities: City[]) {
+        return await fs.writeFile('db.searchHistory.json', JSON.stringify(cities,null,'\t'))
+    }
+
+    async getCities() {
+        return await this.read().then(cities => {
+            let parsedCities: City[];
+            
+            //if can't concat, return empty array
+            try{
+                parsedCities = [].concat(JSON.parse(cities))
+            } catch(err) {
+                parsedCities = [];
+            }
+
+            return parsedCities;
+        })
+    }
+
+    async addCity(city: string) {
+        if(!city) throw new Error('city cannot be blank');
+
+        const newCity = new City(city, uuidv4());
+        
+        return await this.getCities()
+            .then(cities => {
+                if(cities.find(index => index.name === city)) return cities
+                return [...cities,newCity];
+            })
+            .then(cities => this.write(cities))
+            .then(() => newCity);
+    }
+
+    async removeCity(id: string) {
+        if(!id) throw new Error('id cannot be blank')
+
+        await this.getCities()
+            .then(cities => {
+                return cities.filter(city => city.id !== id)
+            })
+            .then(filteredCities => this.write(filteredCities));
+    }
 }
 
 export default new HistoryService();
